b36f386f9873118134c2713ab4349cb22e86dc72
PavloBachysh <happytouni@gmail.com>
2025-03-18 00:33:57 +0200

    Project is done

---
+++ Assets/Scripts/DestroyOutOfBounds.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOutOfBounds : MonoBehaviour
+{
+
+    private float topBounds = 30.0f;
+    private float lowerBounds = -10.0f;
+
+    void Update()
+    {
+        if (transform.position.z > topBounds)
+        {
+            Destroy(gameObject);
+        } else if (transform.position.z < lowerBounds)
+        {
+            Debug.Log("Game Over");
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+
+    public GameObject[] animalPrefabs;
+    private float spawnRangeX = 20.0f;
+    private float spawnPosZ = 20.0f;
+    private float startDelay = 2;
+    private float spawnInterval = 1.5f;
+
+    void Start()
+    {
+        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.S))
+        {
+            SpawnRandomAnimal();
+        }
+    }
+
+    void SpawnRandomAnimal()
+    {
+        int animalIndex = UnityEngine.Random.Range(0, animalPrefabs.Length);
+        Vector3 spawnPos = new Vector3(UnityEngine.Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+
+        Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
+    }
+
+
+}
